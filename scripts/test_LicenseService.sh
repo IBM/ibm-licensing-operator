d4d9c8cb8e801e347f066c5f3894ad0fd2728f0a
martabereta <Marta.Bereta@pl.ibm.com>
2020-08-02 22:46:22 +0200

    Setting env vars for LS url

--- scripts/test_LicenseService.sh
+++ scripts/test_LicenseService.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Tests the connection"
-export LS_URL="http://"$1$2
-echo "LS URL:"$LS_URL
-curl -k -s -w %{http_code} -X GET $LS_URL -o /dev/null
+#export LS_URL="http://"$1$2
+#echo "LS URL:"$LS_URL
+curl -k -s -w %{http_code} -X GET $1 -o /dev/null
