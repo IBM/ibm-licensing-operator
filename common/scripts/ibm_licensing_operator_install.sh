17984101535e1a64e16abc260a64c203db1dc0c2
Adam Dyszy <59733164+adamdyszy@users.noreply.github.com>
2020-11-04 12:16:39 +0100

    Add some features to automatic script (#182)
    * add channel option, try install olm if OLM CRD exists but no OLM csv don't, show token and route url at the end, add option to not show token
    
    * small messages refactor

--- common/scripts/ibm_licensing_operator_install.sh
+++ common/scripts/ibm_licensing_operator_install.sh
@@ -29,9 +29,12 @@ usage()
   echo ""
   echo "note: Use this script only for cluster running on x86 architecture."
   echo ""
-  echo "usage: $0 [--verbose | -v] [--help | -h] [(--olm_version | -o) <version_number>] [--skip_olm_installation | -s] [(--olm_global_catalog_namespace | -c) <OLM global catalog namespace> ]"
+  echo "usage: $0 [--verbose | -v] [--help | -h] [(--olm_version | -o) <version_number>] [--skip_olm_installation | -s] [(--olm_global_catalog_namespace | -c) <OLM global catalog namespace> ] [(--channel | -l) <subscription channel>] [--no-secret-output | -n]"
   echo "options:"
   echo "[--verbose | -v] - verbose logs from installation"
+  echo "[--channel | -l] - do not change unless instructed to. What channel should License Service Opeator subscription choose,"
+  echo "by default channel=stable-v1"
+  echo "[--no-secret-output | -n] - use this option to not show secret at the end of the script"
   echo "[--olm_version | -o] <version_number> - what version of OLM should be installed if it doesn't exist,"
   echo "by default olm_version=0.13.0"
   echo "[--skip_olm_installation | -s] - skips installation of OLM, but olm global catalog namespace still needs to be found."
@@ -97,11 +100,28 @@ install_olm(){
   if [ "${olm_global_catalog_namespace}" == "" ]; then
     verbose_output_command echo "Trying to get namespace where OLM's packageserver is installed"
     if ! olm_namespace=$(kubectl get csv --all-namespaces -l olm.version -o jsonpath="{.items[?(@.metadata.name=='packageserver')].metadata.namespace}") || [ "${olm_namespace}" == "" ]; then
-      echo "Error: Failed to get namespace where OLM's packageserver is installed, which is needed for finding OLM's global catalog namespace, make sure you have OLM installed"
-      echo "You can try to install OLM from here https://github.com/operator-framework/operator-lifecycle-manager/releases"
-      echo "If you can find OLM's global catalog namespace yourself try setting parameter --olm_global_catalog_namespace parameter of this script"
-      echo "On OpenShift Container Platform this probably is 'openshift-marketplace', but for older versions and for custom OLM installation it might be 'olm', but you might verify it by looking for OLM's packageserver deployment configuration"
-      exit 6
+      if [ "${skip_olm_installation}" != "1" ]; then
+        echo "OLM CRD was found but packageserver csv was not found, will try to install OLM with version ${olm_version}"
+        if ! curl -sL https://github.com/operator-framework/operator-lifecycle-manager/releases/download/"${olm_version}"/install.sh | bash -s "${olm_version}"; then
+          echo "Error: Failed to install OLM"
+          echo "You can try to install OLM from here https://github.com/operator-framework/operator-lifecycle-manager/releases and continue installation while skipping OLM part"
+          exit 24
+        fi
+        verbose_output_command echo "Installed OLM ${olm_version}, will try to get olm_namespace again"
+        if ! olm_namespace=$(kubectl get csv --all-namespaces -l olm.version -o jsonpath="{.items[?(@.metadata.name=='packageserver')].metadata.namespace}") || [ "${olm_namespace}" == "" ]; then
+          echo "Error: Failed to get namespace where OLM's packageserver is installed, which is needed for finding OLM's global catalog namespace, make sure you have OLM installed"
+          echo "You can try to install OLM from here https://github.com/operator-framework/operator-lifecycle-manager/releases"
+          echo "If you can find OLM's global catalog namespace yourself try setting parameter --olm_global_catalog_namespace parameter of this script"
+          echo "On OpenShift Container Platform this probably is 'openshift-marketplace', but for older versions and for custom OLM installation it might be 'olm', but you might verify it by looking for OLM's packageserver deployment configuration"
+          exit 25
+        fi
+      else
+        echo "Error: Failed to get namespace where OLM's packageserver is installed, which is needed for finding OLM's global catalog namespace, make sure you have OLM installed"
+        echo "You can try to install OLM from here https://github.com/operator-framework/operator-lifecycle-manager/releases"
+        echo "If you can find OLM's global catalog namespace yourself try setting parameter --olm_global_catalog_namespace parameter of this script"
+        echo "On OpenShift Container Platform this probably is 'openshift-marketplace', but for older versions and for custom OLM installation it might be 'olm', but you might verify it by looking for OLM's packageserver deployment configuration"
+        exit 6
+      fi
     else
       verbose_output_command echo "Namespace where OLM's packageserver is installed is: ${olm_namespace}"
     fi
@@ -152,11 +172,6 @@ EOF
     if [[ $new_cs_state != "$cs_state" ]]; then
       cs_state=$new_cs_state
       echo "opencloud Catalog Source state: $cs_state"
-#      TRANSIENT_FAILURE can switch to READY state
-#      if [ "$cs_state" == "TRANSIENT_FAILURE" ]; then
-#        echo "Error: Problem during installation of Catalog Source, check README for manual installation and troubleshooting"
-#        exit 12
-#      fi
     fi
     sleep 1
     retries=$((retries - 1))
@@ -212,7 +227,7 @@ metadata:
   name: ibm-licensing-operator-app
   namespace: $INSTALL_NAMESPACE
 spec:
-  channel: stable-v1
+  channel: $channel
   name: ibm-licensing-operator-app
   source: opencloud-operators
   sourceNamespace: $olm_global_catalog_namespace
@@ -230,6 +245,12 @@ handle_subscription(){
     verbose_output_command echo "Subscription already exists"
   fi
   echo "Checking Subscription and CSV status"
+  existing_sub_channel=$(kubectl get sub -n "${INSTALL_NAMESPACE}" ibm-licensing-operator-app -o jsonpath='{.spec.channel}')
+  if [[ "$existing_sub_channel" != "$channel" ]]; then
+    echo "Subscription for License Service already exists but have different channel (found: $existing_sub_channel , expected: $channel ),"
+    echo "Either delete existing subscription for License Service Operator or change channel option of the script to the found one"
+    exit 22
+  fi
   retries=55
   no_csv_name_in_sub_count=0
   until [[ $retries == 0 || $new_csv_phase == "Succeeded" ]]; do
@@ -305,7 +326,24 @@ EOF
     echo "Error: IBMLicensing instance pod failed to reach phase Running"
     exit 21
   fi
-  echo "IBM License Service should be running, you can check post installation section in README to see possible configurations of IBM Licensing instance, and how to configure ingress/route if needed"
+}
+
+show_token(){
+  if [ "$no_secret_output" != "1" ]; then
+    if ! licensing_token=$(kubectl get secret ibm-licensing-token -o jsonpath='{.data.token}' -n "$INSTALL_NAMESPACE" | base64 -d) || [ "${licensing_token}" == "" ]; then
+      verbose_output_command echo "Could not get ibm-licensing-token in $INSTALL_NAMESPACE, something might be wrong"
+    else
+      echo "License Service secret for accessing the API is: $licensing_token"
+    fi
+  fi
+}
+
+show_url(){
+  if ! route_url=$(kubectl get route ibm-licensing-service-instance -o jsonpath='{.status.ingress[0].host}' -n "$INSTALL_NAMESPACE") || [ "${route_url}" == "" ]; then
+    verbose_output_command echo "Could not get Route for License Service in $INSTALL_NAMESPACE, Route CRD might not be available at your cluster, or ingress option was chosen"
+  else
+    echo "License Service Route URL for accessing the API is: https://$route_url"
+  fi
 }
 
 verbose_output_command(){
@@ -322,7 +360,8 @@ verbose=
 olm_version=0.13.0
 skip_olm_installation=
 olm_global_catalog_namespace=
-
+channel=stable-v1
+no_secret_output=
 
 while [ "$1" != "" ]; do
   OPT=$1
@@ -338,7 +377,12 @@ while [ "$1" != "" ]; do
     -c | --olm_global_catalog_namespace )               shift
                                                         olm_global_catalog_namespace=$1
                                                         ;;
+    -l | --channel )                                    shift
+                                                        channel=$1
+                                                        ;;
     -s | --skip_olm_installation )                      skip_olm_installation=1
+                                                        ;;
+    -n | --no-secret-output )                           no_secret_output=1
                                                         ;;
     * )                                                 echo "Error: wrong option: $OPT"
                                                         usage
@@ -361,3 +405,6 @@ handle_catalog_source
 handle_operator_group
 handle_subscription
 handle_instance
+show_token
+show_url
+echo "IBM License Service should be running, you can check post installation section in README to see possible configurations of IBM Licensing instance, and how to configure ingress/route if needed"
