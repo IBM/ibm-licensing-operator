5fa7774d1ad9078e7627912497044fc9b6c8a93d
adam <Adam.Dyszy@ibm.com>
2021-01-27 13:03:03 +0100

    add chargeback options to csv and ls crd

--- api/v1alpha1/ibmlicensing_types.go
+++ api/v1alpha1/ibmlicensing_types.go
@@ -98,6 +98,16 @@ type IBMLicensingSpec struct {
 	// +optional
 	RHMPEnabled *bool `json:"rhmpEnabled,omitempty"`
 
+	// Is chargeback enabled
+	// +operator-sdk:csv:customresourcedefinitions:type=spec,displayName="Chargeback Enabled",xDescriptors="urn:alm:descriptor:com.tectonic.ui:text"
+	// +optional
+	ChargebackEnabled *bool `json:"chargebackEnabled,omitempty"`
+
+	// For how many days chargeback grouped data will be stored in config maps, this parameter can be adjusted to ensure config map will not overflow, by default 62 days
+	// +operator-sdk:csv:customresourcedefinitions:type=spec,displayName="Chargeback Retention Period in days",xDescriptors="urn:alm:descriptor:com.tectonic.ui:number"
+	// +optional
+	ChargebackRetentionPeriod *int `json:"chargebackRetentionPeriod,omitempty"`
+
 	// Should Ingress be created to expose IBM Licensing Service API?
 	// +operator-sdk:csv:customresourcedefinitions:type=spec,displayName="Ingress Enabled",xDescriptors="urn:alm:descriptor:com.tectonic.ui:text"
 	// +optional
