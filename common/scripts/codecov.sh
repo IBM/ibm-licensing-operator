90f1f38e4df68cbc3b4e5a728e72d57049e18c12
aobrzut <artur.obrzut@pl.ibm.com>
2021-02-05 11:09:39 +0100

    coverage

--- common/scripts/codecov.sh
+++ common/scripts/codecov.sh
@@ -54,12 +54,10 @@ function code_coverage() {
   local filename
   local count=${2:-0}
   filename="$(echo "${1}" | tr '/' '-')"
-  export USE_EXISTING_CLUSTER=true; \
-  export WATCH_NAMESPACE=${NAMESPACE}; \
   export NAMESPACE=${NAMESPACE}; \
+  export WATCH_NAMESPACE=${NAMESPACE}; \
+  export USE_EXISTING_CLUSTER=true; \
   export KUBEBUILDER_ATTACH_CONTROL_PLANE_OUTPUT=true; \
-  export REGISTRY=${REGISTRY}; \
-  export CSV_VERSION=${CSV_VERSION}; \
   export IBM_LICENSING_IMAGE=${IBM_LICENSING_IMAGE}; \
   export IBM_LICENSE_SERVICE_REPORTER_IMAGE=${IBM_LICENSE_SERVICE_REPORTER_IMAGE}; \
   export IBM_LICENSE_SERVICE_REPORTER_UI_IMAGE=${IBM_LICENSE_SERVICE_REPORTER_UI_IMAGE}; \
