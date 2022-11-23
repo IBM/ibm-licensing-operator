469ea8914a74c9e227ec5b9731ff8963281996c4
Jan Olszowka <jan.olszowka@ibm.com>
2022-11-23 10:29:28 +0100

    Updated prow cluster name

--- common/Makefile.common.mk
+++ common/Makefile.common.mk
@@ -16,8 +16,8 @@
 # GKE section
 ############################################################
 PROJECT ?= oceanic-guard-191815
-ZONE    ?= us-west1-a
-CLUSTER ?= prow
+ZONE    ?= us-east5-c
+CLUSTER ?= bedrock-prow
 
 activate-serviceaccount:
 ifdef GOOGLE_APPLICATION_CREDENTIALS
