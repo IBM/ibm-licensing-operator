93511190bd433afbd37578f8ac1dd81f56ce868e
piotr-kotara-ibm <92912677+piotr-kotara-ibm@users.noreply.github.com>
2022-12-13 15:46:19 +0100

    certificate hardening (#702) (#715)
    Signed-off-by: Piotr Kotara <piotr.kotara@ibm.com>
    Co-authored-by: SzymonKowalczyk <szymon.kowalczyk@pl.ibm.com>
    Co-authored-by: adam <Adam.Dyszy@ibm.com>
    Co-authored-by: Jan Olszowka <jan.olszowka@ibm.com>

--- controllers/resources/reporter/helper.go
+++ controllers/resources/reporter/helper.go
@@ -30,7 +30,6 @@ import (
 	operatorv1alpha1 "github.com/IBM/ibm-licensing-operator/api/v1alpha1"
 	res "github.com/IBM/ibm-licensing-operator/controllers/resources"
 	"github.com/IBM/ibm-licensing-operator/version"
-
 	odlm "github.com/IBM/operand-deployment-lifecycle-manager/api/v1alpha1"
 )
 
@@ -89,19 +88,10 @@ func LabelsForPod(instance *operatorv1alpha1.IBMLicenseServiceReporter) map[stri
 }
 
 func getReciverEnvVariables(spec operatorv1alpha1.IBMLicenseServiceReporterSpec) []corev1.EnvVar {
-	var certSource operatorv1alpha1.HTTPSCertsSource
-
-	if res.IsServiceCAAPI {
-		certSource = operatorv1alpha1.OcpCertsSource
-	} else {
-		// on K8s always always generate self-signed certificate in operand
-		certSource = operatorv1alpha1.SelfSignedCertsSource
-	}
-
 	environmentVariables := []corev1.EnvVar{
 		{
 			Name:  "HTTPS_CERTS_SOURCE",
-			Value: string(certSource),
+			Value: string(operatorv1alpha1.ExternalCertsSource),
 		},
 	}
 	if spec.IsDebug() {
