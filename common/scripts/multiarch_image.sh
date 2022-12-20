59aeafe18d15901f7f5794ba7d4b10bd3a1999d9
Jan-Olszowka <111862576+Jan-Olszowka@users.noreply.github.com>
2022-12-20 15:00:00 +0100

    Fix docker tag multiarch (#727)
    * Added fix for invalid branch names
    
    * Changed sed to build in replacement
    
    * Trigger new build

--- common/scripts/multiarch_image.sh
+++ common/scripts/multiarch_image.sh
@@ -61,8 +61,9 @@ ${CONTAINER_CLI} manifest push "${IMAGE_REPO}"/"${IMAGE_NAME}":"${MANIFEST_VERSI
 
 if [ -n "$ADDITIONAL_TAG" ]
 then
+  ADDITIONAL_TAG="${ADDITIONAL_TAG//[^[:alnum:]\.\_\-]/-}"
   ${CONTAINER_CLI} pull "${IMAGE_REPO}"/"${IMAGE_NAME}":"${MANIFEST_VERSION}"
-    echo "Tagging multi-arch image manifest for ${IMAGE_REPO}/${IMAGE_NAME}:${ADDITIONAL_TAG} with branch name..."
+  echo "Tagging multi-arch image manifest for ${IMAGE_REPO}/${IMAGE_NAME}:${ADDITIONAL_TAG} with branch name..."
   ${CONTAINER_CLI} tag "${IMAGE_REPO}"/"${IMAGE_NAME}":"${MANIFEST_VERSION}" "${IMAGE_REPO}"/"${IMAGE_NAME}":"${ADDITIONAL_TAG}"
   ${CONTAINER_CLI} push "${IMAGE_REPO}"/"${IMAGE_NAME}":"${ADDITIONAL_TAG}"
 fi
