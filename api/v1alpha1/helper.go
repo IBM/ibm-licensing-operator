073653ad8aa5e2f05c735e34af79ab4d8ace3890
marta.bereta <marta.bereta@pl.ibm.com>
2023-03-13 12:29:55 +0100

    Removal variables for reporter only

--- api/v1alpha1/helper.go
+++ api/v1alpha1/helper.go
@@ -17,7 +17,6 @@
 package v1alpha1
 
 import (
-	"context"
 	"errors"
 	"fmt"
 	"os"
@@ -28,9 +27,7 @@ import (
 	"github.com/go-logr/logr"
 	routev1 "github.com/openshift/api/route/v1"
 	corev1 "k8s.io/api/core/v1"
-	storagev1 "k8s.io/api/storage/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
-	client_reader "sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 const localReporterURL = "https://ibm-license-service-reporter:8080"
@@ -45,13 +42,9 @@ var memory64Mi = resource.NewQuantity(64*1024*1024, resource.BinarySI)
 var memory128Mi = resource.NewQuantity(128*1024*1024, resource.BinarySI)
 
 var cpu200m = resource.NewMilliQuantity(200, resource.DecimalSI)
-var cpu300m = resource.NewMilliQuantity(300, resource.DecimalSI)
 var memory256Mi = resource.NewQuantity(256*1024*1024, resource.BinarySI)
-var memory300Mi = resource.NewQuantity(256*1024*1024, resource.BinarySI)
-var memory384Mi = resource.NewQuantity(384*1024*1024, resource.BinarySI)
 var cpu500m = resource.NewMilliQuantity(500, resource.DecimalSI)
 var memory512Mi = resource.NewQuantity(512*1024*1024, resource.BinarySI)
-var size1Gi = resource.NewQuantity(1024*1024*1024, resource.BinarySI)
 
 type Container struct {
 	// IBM Licensing Service docker Image Registry, will override default value and disable IBM_LICENSING_IMAGE env value in operator deployment
@@ -210,37 +203,6 @@ func (spec *IBMLicensingSpec) IsChargebackEnabled() bool {
 		return true
 	}
 	return spec.ChargebackEnabled != nil && *spec.ChargebackEnabled
-}
-
-func getStorageClass(reqLogger logr.Logger, r client_reader.Reader) (string, error) {
-	var defaultSC []string
-
-	scList := &storagev1.StorageClassList{}
-	reqLogger.Info("getStorageClass")
-	err := r.List(context.TODO(), scList)
-	if err != nil {
-		return "", err
-	}
-	if len(scList.Items) == 0 {
-		return "", fmt.Errorf("could not find storage class in the cluster")
-	}
-
-	for _, sc := range scList.Items {
-		if sc.Provisioner == "kubernetes.io/no-provisioner" {
-			continue
-		}
-		if sc.ObjectMeta.GetAnnotations()["storageclass.kubernetes.io/is-default-class"] == "true" {
-			defaultSC = append(defaultSC, sc.GetName())
-			continue
-		}
-	}
-
-	if len(defaultSC) != 0 {
-		reqLogger.Info("StorageClass configuration", "Name", defaultSC[0])
-		return defaultSC[0], nil
-	}
-
-	return "", fmt.Errorf("could not find dynamic provisioner default storage class in the cluster")
 }
 
 func (container *Container) initResourcesIfNil() {
