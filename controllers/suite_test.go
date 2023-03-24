9425f0154a7043e801d19706f0883344c88cc73f
marta.bereta <marta.bereta@pl.ibm.com>
2023-03-24 12:48:30 +0100

    Fixing package manifest name and removed tests

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -19,6 +19,7 @@ package controllers
 import (
 	"os"
 	"path/filepath"
+	"testing"
 	"time"
 
 	monitoringv1 "github.com/coreos/prometheus-operator/pkg/apis/monitoring/v1"
@@ -34,6 +35,7 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
+	"sigs.k8s.io/controller-runtime/pkg/envtest/printer"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
@@ -55,6 +57,14 @@ var (
 	timeout   = time.Second * 300
 	interval  = time.Second * 5
 )
+
+func TestAPIs(t *testing.T) {
+	RegisterFailHandler(Fail)
+
+	RunSpecsWithDefaultAndCustomReporters(t,
+		"Controller Suite",
+		[]Reporter{printer.NewlineReporter{}})
+}
 
 var _ = BeforeSuite(func(done Done) {
 
