e2eeb9b00e518f0b116230a50f28a127396923fb
Jan Olszowka <jan.olszowka@ibm.com>
2024-01-04 11:55:28 +0100

    Change subpath for correct one - token

--- controllers/resources/service/secrets.go
+++ controllers/resources/service/secrets.go
@@ -28,7 +28,7 @@ const APIUploadTokenName = "ibm-licensing-upload-token"
 const APISecretTokenKeyName = "token"
 const APIUploadTokenKeyName = "token-upload"
 
-const ReporterSecretTokenKeyName = "reporter-token"
+const ReporterSecretTokenKeyName = "token"
 
 const URLConfigMapKey = "url"
 const CrtConfigMapKey = "crt.pem"
