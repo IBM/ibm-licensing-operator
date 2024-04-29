27b0fd5db7f1f3ebd135642ca077ba08fdc156f4
Kacper Florianski <k.florianski@ibm.com>
2024-04-29 20:26:39 +0200

    - Added custom annotations support - Upgraded controller-gen and operator-sdk versions - Removed some unused functions and variables
    Signed-off-by: Kacper Florianski <k.florianski@ibm.com>

--- controllers/resources/service/secrets.go
+++ controllers/resources/service/secrets.go
@@ -17,24 +17,28 @@
 package service
 
 import (
+	"crypto/rand"
+	"math/big"
+
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	operatorv1alpha1 "github.com/IBM/ibm-licensing-operator/api/v1alpha1"
-	"github.com/IBM/ibm-licensing-operator/controllers/resources"
 )
 
 const APIUploadTokenName = "ibm-licensing-upload-token"
 const APISecretTokenKeyName = "token"
 const APIUploadTokenKeyName = "token-upload"
 
-const ReporterSecretTokenKeyName = "token"
-
 const URLConfigMapKey = "url"
 const CrtConfigMapKey = "crt.pem"
 
 //goland:noinspection GoNameStartsWithPackageName
 const ServiceAccountSecretAnnotationKey = "kubernetes.io/service-account.name"
+
+const randStringCharset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+var randStringCharsetLength = big.NewInt(int64(len(randStringCharset)))
 
 func GetDefaultReaderToken(instance *operatorv1alpha1.IBMLicensing) (*corev1.Secret, error) {
 	expectedSecret := &corev1.Secret{
@@ -61,20 +65,60 @@ func GetServiceAccountSecret(instance *operatorv1alpha1.IBMLicensing) (*corev1.S
 }
 
 func GetAPISecretToken(instance *operatorv1alpha1.IBMLicensing) (*corev1.Secret, error) {
-	return resources.GetSecretToken(instance.Spec.APISecretToken, instance.Spec.InstanceNamespace, APISecretTokenKeyName, LabelsForMeta(instance))
+	return getSecretToken(
+		instance,
+		instance.Spec.APISecretToken,
+		APISecretTokenKeyName,
+	)
 }
 
 func GetUploadToken(instance *operatorv1alpha1.IBMLicensing) (*corev1.Secret, error) {
-	return resources.GetSecretToken(APIUploadTokenName, instance.Spec.InstanceNamespace, APIUploadTokenKeyName, LabelsForMeta(instance))
+	return getSecretToken(
+		instance,
+		APIUploadTokenName,
+		APIUploadTokenKeyName,
+	)
+}
+
+func getSecretToken(instance *operatorv1alpha1.IBMLicensing, name string, secretKey string) (*corev1.Secret, error) {
+	randStringValue, err := randString(24)
+	if err != nil {
+		return nil, err
+	}
+	expectedSecret := &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        name,
+			Namespace:   instance.Spec.InstanceNamespace,
+			Labels:      LabelsForMeta(instance),
+			Annotations: instance.Spec.Annotations,
+		},
+		Type:       corev1.SecretTypeOpaque,
+		StringData: map[string]string{secretKey: randStringValue},
+	}
+	return expectedSecret, nil
+}
+
+func randString(length int) (string, error) {
+	reader := rand.Reader
+	outputStringByte := make([]byte, length)
+	for i := 0; i < length; i++ {
+		charIndex, err := rand.Int(reader, randStringCharsetLength)
+		if err != nil {
+			return "", err
+		}
+		outputStringByte[i] = randStringCharset[charIndex.Int64()]
+	}
+	return string(outputStringByte), nil
 }
 
 func GetUploadConfigMap(instance *operatorv1alpha1.IBMLicensing, internalCertData string) *corev1.ConfigMap {
 	metaLabels := LabelsForMeta(instance)
 	expectedCM := &corev1.ConfigMap{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      "ibm-licensing-upload-config",
-			Namespace: instance.Spec.InstanceNamespace,
-			Labels:    metaLabels,
+			Name:        "ibm-licensing-upload-config",
+			Namespace:   instance.Spec.InstanceNamespace,
+			Labels:      metaLabels,
+			Annotations: instance.Spec.Annotations,
 		},
 		Data: map[string]string{
 			URLConfigMapKey: GetServiceURL(instance),
@@ -88,9 +132,10 @@ func GetInfoConfigMap(instance *operatorv1alpha1.IBMLicensing) *corev1.ConfigMap
 	metaLabels := LabelsForMeta(instance)
 	expectedCM := &corev1.ConfigMap{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      "ibm-licensing-info",
-			Namespace: instance.Spec.InstanceNamespace,
-			Labels:    metaLabels,
+			Name:        "ibm-licensing-info",
+			Namespace:   instance.Spec.InstanceNamespace,
+			Labels:      metaLabels,
+			Annotations: instance.Spec.Annotations,
 		},
 		Data: map[string]string{URLConfigMapKey: GetServiceURL(instance)},
 	}
