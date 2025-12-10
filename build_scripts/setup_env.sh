2aaee42021e5d68ad63719d1d587a31066a1377a
Michal Szczygiel <Michal.Szczygiel@ibm.com>
2025-12-10 16:55:10 +0100

    Go version set

--- build_scripts/setup_env.sh
+++ build_scripts/setup_env.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 # Tool versions:
-export GO_VERSION="1.24"
+export GO_VERSION="1.24.10"
 export GOLANGCI_LINT_VERSION="1.64.2"
 export OPERATOR_SDK_VERSION="v1.25.2"
 export KIND_VERSION="v0.17.0"
