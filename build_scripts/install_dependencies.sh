340eae955e933fb1f971be518513cecbf0bf1a96
Michal Szczygiel <Michal.Szczygiel@ibm.com>
2026-03-20 12:16:59 +0100

    Change 2023 -> 2026 in freshly modified files

--- build_scripts/install_dependencies.sh
+++ build_scripts/install_dependencies.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Copyright 2023 IBM Corporation
+# Copyright 2026 IBM Corporation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
