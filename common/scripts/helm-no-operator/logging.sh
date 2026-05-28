cd24750bb2d8db13c1593f3f9c93dfde82b6ff35
Norbert Koziana <norbert.koziana@ibm.com>
2026-05-28 15:10:58 +0200

    Refactor logging

--- common/scripts/helm-no-operator/logging.sh
+++ common/scripts/helm-no-operator/logging.sh
@@ -24,21 +24,21 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 BLUE='\033[0;34m'
-NC='\033[0m' # No Color
+NO_COLOR='\033[0m'
 
 # Logging functions
 log_info() {
-    echo -e "${GREEN}[INFO]${NC} $1"
+    echo -e "${GREEN}[INFO]${NO_COLOR} $1"
 }
 
 log_warn() {
-    echo -e "${YELLOW}[WARN]${NC} $1"
+    echo -e "${YELLOW}[WARN]${NO_COLOR} $1"
 }
 
 log_error() {
-    echo -e "${RED}[ERROR]${NC} $1"
+    echo -e "${RED}[ERROR]${NO_COLOR} $1"
 }
 
 log_step() {
-    echo -e "${BLUE}[STEP]${NC} $1"
+    echo -e "${BLUE}[STEP]${NO_COLOR} $1"
 }
