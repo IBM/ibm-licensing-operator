8453c61343aa9f8dcaed931215becc6d89939531
Michal Szczygiel <Michal.Szczygiel@ibm.com>
2026-06-22 10:27:47 +0200

    Implement whitespace trimming and deduplication

--- controllers/resources/envs_test.go
+++ controllers/resources/envs_test.go
@@ -18,11 +18,70 @@ package resources
 
 import (
 	"os"
+	"reflect"
 	"strconv"
 	"strings"
 	"testing"
 	"time"
 )
+
+func TestGetWatchNamespaceAsList(t *testing.T) {
+	envVar := "WATCH_NAMESPACE"
+
+	t.Log("Given the need to read and sanitize the WATCH_NAMESPACE environment variable")
+	{
+		testCases := []struct {
+			name     string
+			value    string
+			expected []string
+		}{
+			{"single namespace", "ibm-licensing", []string{"ibm-licensing"}},
+			{"multiple namespaces", "ns1,ns2,ns3", []string{"ns1", "ns2", "ns3"}},
+			{"surrounding whitespace is trimmed", " ns1 , ns2 ", []string{"ns1", "ns2"}},
+			{"empty entries are skipped", "ns1,,ns2,", []string{"ns1", "ns2"}},
+			{"whitespace-only entries are skipped", "ns1, ,ns2", []string{"ns1", "ns2"}},
+			{"duplicates are removed", "ns1,ns2,ns1", []string{"ns1", "ns2"}},
+			{"duplicates after trimming are removed", "ns1, ns1 ,ns2", []string{"ns1", "ns2"}},
+			{"input order is preserved", "ns3,ns1,ns2", []string{"ns3", "ns1", "ns2"}},
+			{"empty value yields empty list", "", []string{}},
+			{"only separators yields empty list", ",,,", []string{}},
+		}
+
+		for i, tc := range testCases {
+			t.Logf("\tTest %d:\t%s", i, tc.name)
+			{
+				t.Setenv(envVar, tc.value)
+
+				actual, err := GetWatchNamespaceAsList()
+				if err != nil {
+					t.Fatalf("\t%s\tShould not get an error : %v", FAIL, err)
+				}
+				if reflect.DeepEqual(actual, tc.expected) {
+					t.Logf("\t%s\tShould get %v", SUCCESS, tc.expected)
+				} else {
+					t.Errorf("\t%s\tShould get %v : got %v", FAIL, tc.expected, actual)
+				}
+			}
+		}
+
+		t.Log("\tTest:\tWhen env var is not set")
+		{
+			os.Unsetenv(envVar)
+
+			_, err := GetWatchNamespaceAsList()
+			if err == nil {
+				t.Fatalf("\t%s\tShould get an error when %s is unset", FAIL, envVar)
+			}
+
+			errMsg := envVar + " must be set"
+			if strings.Contains(err.Error(), errMsg) {
+				t.Logf("\t%s\tShould get an error with a proper message", SUCCESS)
+			} else {
+				t.Errorf("\t%s\tShould get an error with a proper message : %s", FAIL, "\""+errMsg+"\"")
+			}
+		}
+	}
+}
 
 func TestGetReconcileInterval(t *testing.T) {
 	envVar := "CRD_RECONCILE_INTERVAL"
